863b595e4d737eecf9eb2520c7a70217f42156a9
Isac Reis <aristocrata.black>
2026-03-06 21:51:49 -0300

    gfasdad

--- client/next.config.ts
+++ client/next.config.ts
@@ -6,11 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4000/api/:path*',
-      },
-      {
-        source: '/health',
-        destination: 'http://localhost:4000/health',
+        destination: 'http://127.0.0.1:4000/api/:path*',
       },
     ];
   },
